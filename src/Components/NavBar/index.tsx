d1ce8afd2d33d618aa0bae1e6bcd5a9454392998
Tom Groombridge <tom.groombridge@zopa.com>
2018-10-18 19:52:07 +0100

    changed href location

--- src/Components/NavBar/index.tsx
+++ src/Components/NavBar/index.tsx
@@ -18,7 +18,7 @@ const NavBar = (props: any) => {
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
+        <a className="navbar-item" href="/">
           <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
         </a>
 
