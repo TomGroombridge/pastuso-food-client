14d431aea9eed8e7e61fc2d026b44448f6ebd4df
Tom Groombridge <tom.groombridge@zopa.com>
2018-10-18 15:03:19 +0100

    added a rough navbar with login and logout buttons

--- src/App/Home/index.tsx
+++ src/App/Home/index.tsx
@@ -1,30 +1,13 @@
 import * as React from 'react'
+import NavBar from '../../Components/NavBar';
 
-const Home = (props: any) => {  
-
-  const openLogin = () => {    
-    props.auth.login();    
-  }
-
-  const logout = () => {    
-    props.auth.logout();
-  }
-
-  const { isAuthenticated } = props.auth;
-
+const Home = (props: any) => {    
   return(
     <>
-      <h1>This is the home page!</h1>
-      { 
-        isAuthenticated() && (
-          <button onClick={() => logout()}>Log out</button>    
-        )
-      }
-      { 
-        !isAuthenticated() && (
-          <button onClick={() => openLogin()}>Log in</button>
-        )
-      }      
+      <NavBar auth={props.auth}/>
+      <section className="section">
+        <h1>This is a recipe</h1>        
+      </section>
     </>
   )
 }
