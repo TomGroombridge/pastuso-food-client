3c0d53ed3eb5454406da566aea38e3592878be37
Tom Groombridge <groombridgetom@gmail.com>
2018-10-30 19:14:33 +0000

    Add json structure for a recipe (#6)
    * added rough idea of what recipe.json shuold look like
    
    * added recipe json
    
    * replaced static content with file loaded from json file and set in state
    
    * remove paddingless class
    
    * added some more values to a recipe
    
    * added comment

--- src/App/Recipes/index.tsx
+++ src/App/Recipes/index.tsx
@@ -1,19 +1,20 @@
 import * as React from 'react';
 import RecipeTile from '../../Components/RecipeTile';
+// we should have a json file to show all the recipes we have here.
 
-const Recipes = (props: any) => {  
+const Recipes = (props: any) => {
   return(
-    <section className="section">    
+    <section className="section">
       <div className="tile is-ancestor">
         <div className="tile is-vertical is-12 ">
           <div className="tile">
             <RecipeTile history={props.history}/>
             <RecipeTile history={props.history}/>
             <RecipeTile history={props.history}/>
-            <RecipeTile history={props.history}/>            
+            <RecipeTile history={props.history}/>
           </div>
         </div>
-      </div>        
+      </div>
     </section>
   )
 }
