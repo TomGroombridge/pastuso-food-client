ee96f80824759782ace8e585bbd3b50b8f5fa9d3
Tom Groombridge <tom.groombridge@zopa.com>
2018-10-31 18:47:57 +0000

    added some rough styling to the index page of recipes

--- src/Components/RecipeTile/index.tsx
+++ src/Components/RecipeTile/index.tsx
@@ -1,12 +1,25 @@
 import * as React from 'react';
+import Photo from '../../Styles/FoodPicture.jpg' ;
 
 const RecipeTile = ({history}: any) => {
-  return(    
-    <div className="tile is-parent is-vertical" onClick={() => history.push('/recipes/1')}>
-      <article className="tile is-child notification is-primary">
-        <p className="title">Vertical...</p>
-        <p className="subtitle">Top tile</p>
-      </article>              
+  return(
+    <div className="column is-3">
+      <div className="card" style={{cursor: 'pointer'}} onClick={() => history.push('recipes/1')}>
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img src={Photo} alt="Placeholder image"/>
+          </figure>
+        </div>
+        <div className="card-content">
+          <p className="title is-4">Lasagne</p>
+          <p className="subtitle is-6">
+            <div className="tags">
+              <span className="tag">Family Favourite</span>
+              <span className="tag">Freezable</span>
+            </div>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
