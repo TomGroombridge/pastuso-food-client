bfcbb1b0279aa7988ec9442296085db8c1ed11aa
Tom Groombridge <tom.groombridge@zopa.com>
2018-11-20 22:05:19 +0000

    added recipe json to reducer

--- src/App/RecipesSteps/sagas.ts
+++ src/App/RecipesSteps/sagas.ts
@@ -1,13 +1,13 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import { LOADING } from '../Loader/actionTypes';
+import { FETCH_RECIPE, FETCH_RECIPE_SUCCESS } from './actionTypes';
 
-import {
-  FETCH_RECIPE
-} from './actionTypes';
+import * as recipeJson from "../../Data/recipe.json";
 
 export function* fetchRecipeSaga(){
   try {
     yield put({ type: LOADING, payload: true });
+    yield put({ type: FETCH_RECIPE_SUCCESS, payload: recipeJson })
   } catch {
     yield put({ type: LOADING, payload: false });
   }
--- src/Redux/rootReducer.ts
+++ src/Redux/rootReducer.ts
@@ -1,8 +1,10 @@
 import { routerReducer } from 'react-router-redux';
 import { combineReducers } from 'redux';
 import loader from '../App/Loader/reducer';
+import recipeSteps from '../App/RecipesSteps/reducer';
 
 export default combineReducers({
   loader,
+  recipeSteps,
   router: routerReducer,
 });
